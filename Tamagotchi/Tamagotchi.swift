9a34fd6c1985255d03cd7580f98a44560833a33a
Ssnehnil Ghosh <ghosh.s@etoncollege.org.uk>
2021-11-18 13:09:06 +0000

    Two tests completed.

--- Tamagotchi/Tamagotchi.swift
+++ Tamagotchi/Tamagotchi.swift
@@ -8,10 +8,20 @@
 import Foundation
 
 class Tamagotchi {
-    var hunger = 5
+    var hunger: Int
+    var name: String
+    
+    init(name: String) {
+        hunger = 5
+        self.name = name
+    }
     
     func eatSnack() {
-        hunger -= 1
+        if hunger == 0 {
+            hunger = 0
+        } else {
+            hunger -= 1
+        }
     }
     
 }
--- TamagotchiUnitTests/TamagotchiTest.swift
+++ TamagotchiUnitTests/TamagotchiTest.swift
@@ -11,11 +11,22 @@ class TamagotchiTest: XCTestCase {
 
     func testEatSnackReducesHungerBy1() {
         //arrange
-        let tamagotchi = Tamagotchi()
+        let tamagotchi = Tamagotchi(name: "Ssnehnil")
         //act
+        tamagotchi.hunger = 5
         tamagotchi.eatSnack()
         //assert
         XCTAssertEqual(tamagotchi.hunger, 4)
+    }
+    
+    func testEatSnackWhenNotHungry() {
+        //arrange
+        let tamagotchi = Tamagotchi(name: "Ssnehnil")
+        //act
+        tamagotchi.hunger = 0
+        tamagotchi.eatSnack()
+        //assert
+        XCTAssertEqual(tamagotchi.hunger, 0)
     }
 
 }
